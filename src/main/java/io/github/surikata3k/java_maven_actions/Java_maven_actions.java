d5f8c3cbcc442e0c9814a7c4d5aca71c0bb7340c
surikata3k@gmail.com <surikata3k@gmail.com>
2025-03-12 20:22:05 +0100

    First file from App1

--- src/main/java/io/github/surikata3k/java_maven_actions/Java_maven_actions.java
+++ src/main/java/io/github/surikata3k/java_maven_actions/Java_maven_actions.java
@@ -11,6 +11,6 @@ package io.github.surikata3k.java_maven_actions;
 public class Java_maven_actions {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
