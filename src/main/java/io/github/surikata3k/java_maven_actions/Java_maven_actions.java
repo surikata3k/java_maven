263d0f96e463d3a1b8e6185a8ea44804160d369a
surikata3k@gmail.com <surikata3k@gmail.com>
2025-03-12 20:19:24 +0100

    First file from App1

---
+++ src/main/java/io/github/surikata3k/java_maven_actions/Java_maven_actions.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.surikata3k.java_maven_actions;
+
+/**
+ *
+ * @author roger
+ */
+public class Java_maven_actions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
